c8d4324c41c186961d68f8a6e189d04d656c345b
KangLeian <rayhannabhanlukmansyah@gmail.com>
2022-07-01 17:49:30 +0700

    version

--- version.php
+++ version.php
@@ -30,6 +30,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version    = 2022051800;
 $plugin->requires   = 2020110900; // Moodle Version 3.10 (could change depending if not supported)
 $plugin->maturity   = MATURITY_STABLE;
-$plugin->release    = '1.0';
+$plugin->release    = '1.0.0';
 $plugin->component  = 'enrol_duitku';
 $plugin->cron       = 60;
